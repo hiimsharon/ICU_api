461f05019b9c288ea169f484efdef41011555986
Sha <108519055+hiimsharon@users.noreply.github.com>
2025-06-17 01:40:24 +0800

    刪除多餘const moogoo

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 console.log("🔍 Using MongoDB URI:", process.env.MONGODB_URI);
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
